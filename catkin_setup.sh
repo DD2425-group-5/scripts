3f2fea3db4f25c6a1f47fe5e0aa7141bba161e06
Michal Staniaszek <m.staniaszek@gmail.com>
2014-10-16 15:25:58 +0200

    Added symbolic linking of arduino messages

--- catkin_setup.sh
+++ catkin_setup.sh
@@ -6,4 +6,12 @@ catkin_init_workspace # initialise the workspace
 cd ~/catkin_ws
 catkin_make # make the various bits that are in the workspace
 source devel/setup.bash
-echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc # load the new setup file on bash load
+# make a symbolic link to the arduino messages so that they can be found by the compiler on
+# the current user.
+ln -s /home/ras/catkin_ws/src/ras_arduino_msgs/ ~/catkin_ws/src
+cd ~/catkin_ws
+# run catkin make to compile the arduino messages. Crashes the first
+# time, so run twice?
+catkin_make
+catkin_make
