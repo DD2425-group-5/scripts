4f631e091d9ab607979d7fbc54fdb967200b8eb8
Michal Staniaszek <m.staniaszek@gmail.com>
2014-10-17 15:42:50 +0200

    Modifying ROS_PACKAGE_PATH in catkin setup

--- catkin_setup.sh
+++ catkin_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# This script sets up a catkin workspace in the specified directory (should be a home directory),
-# and then adds a line to .bashrc to allow the use of ros commands. The arduino_msgs package is
-# also symlinked to the home directory and then compiled so that it can be used.
+# This script sets up a catkin workspace in the specified directory (should be a
+# home directory), and then adds a line to .bashrc to allow the use of ros
+# commands. The arduino_msgs package is also symlinked to the home directory and
+# then compiled so that it can be used.
 if [ "$#" -eq 0 ]; then
     HOME=~
 else
@@ -17,9 +18,12 @@ catkin_init_workspace # initialise the workspace
 cd $HOME/catkin_ws
 catkin_make # make the various bits that are in the workspace
 source devel/setup.bash
-echo "source $HOME/catkin_ws/devel/setup.bash" >> $HOME/.bashrc # load the new setup file on bash load
-# make a symbolic link to the arduino messages so that they can be found by the compiler on
-# the current user.
+# load the new setup file on bash load
+echo "source $HOME/catkin_ws/devel/setup.bash" >> $HOME/.bashrc
+# update the package path so that stuff in catkin is used
+echo "ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/catkin_ws/src"
+# make a symbolic link to the arduino messages so that they can be found by the
+# compiler on the current user.
 ln -s /home/ras/catkin_ws/src/ras_arduino_msgs/ $HOME/catkin_ws/src
 cd $HOME/catkin_ws
 # run catkin make to compile the arduino messages. Crashes the first
